923693db386fd6f2c26f3e519c16d5e8d96b6830
thomasdelauro22 <thomasdelauro22@gmail.com>
2023-09-02 19:07:16 -0400

    switch to 10x10 board

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,29 @@ import Gameboard from "@/components/Gameboard";
 
 export default function Home() {
   const board = new Board();
-  const boardWidth = 5;
-  const boardHeight = 5;
+  const boardWidth = 10;
+  const boardHeight = 10;
   const hints = [
-    ["2"],
-    ["3"],
-    ["1"],
+    ["3", "2"],
     ["2", "2"],
-    ["2", "1"],
-    ["3"],
-    ["1", "2"],
-    ["2"],
-    ["1", "1"],
-    ["1", "2"],
+    ["1", "4"],
+    ["5"],
+    ["5", "1"],
+    ["6", "1"],
+    ["2", "4"],
+    ["5"],
+    ["7"],
+    ["6"],
+    ["3", "1"],
+    ["2", "2"],
+    ["1", "4"],
+    ["5", "1"],
+    ["6", "2"],
+    ["6", "3"],
+    ["1", "1", "4"],
+    ["4"],
+    ["4"],
+    ["6"]
   ];
   board.initialize(boardWidth, boardHeight);
 
