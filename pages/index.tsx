41d9fb980b3282d69e8846dcc8cc1b5c44d22545
thomasdelauro22 <thomasdelauro22@gmail.com>
2023-10-03 21:38:53 -0400

    add new file for preset puzzles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,17 @@
 import Button from "@/components/Button";
 import Gameboard from "@/components/Gameboard";
 import { Board } from "../types/Board";
+import { hints1, hints2 } from "@/puzzles";
 
 export default function Home() {
   const board = new Board();
   const boardWidth = 10;
   const boardHeight = 10;
-  const hints = [
-    ["3", "2"],
-    ["2", "2"],
-    ["1", "4"],
-    ["5"],
-    ["5", "1"],
-    ["6", "1"],
-    ["2", "4"],
-    ["5"],
-    ["7"],
-    ["6"],
-    ["3", "1"],
-    ["2", "2"],
-    ["1", "4"],
-    ["5", "1"],
-    ["6", "2"],
-    ["6", "3"],
-    ["1", "1", "4"],
-    ["4"],
-    ["4"],
-    ["6"],
-  ];
   board.initialize(boardWidth, boardHeight);
 
   return (
     <div className="h-screen w-screen py-64">
-      <Gameboard width={boardWidth} height={boardHeight} hints={hints} />
+      <Gameboard width={boardWidth} height={boardHeight} hints={hints2} />
     </div>
   );
 }
